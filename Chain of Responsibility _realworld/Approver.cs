0578a18d2866eb0f667d985dbc69c349e7043fb7
lydpm225442-droid <ly_dpm225442@student.agu.edu.vn>
2025-09-30 09:49:11 +0700

    Add files via upload

---
+++ Chain of Responsibility _realworld/Approver.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// The 'Handler' abstract class
+/// </summary>
+public abstract class Approver
+{
+    protected Approver successor;
+    public void SetSuccessor(Approver successor)
+    {
+        this.successor = successor;
+    }
+    public abstract void ProcessRequest(Purchase purchase);
+}
